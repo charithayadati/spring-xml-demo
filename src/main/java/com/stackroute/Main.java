b89336bbb0f67d064d360f040c1de2182dfe61ed
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 10:01:41 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -15,22 +15,5 @@ public class Main {
         System.out.println("By using autowiring byName  ");
         film.actorDetails();
 
-
-       /* Movie film1 = context.getBean("movie1", Movie.class);
-        film1.actorDetails();
-        //showing the equality of beans
-        System.out.println(film==film1);
-        Movie film2 = context.getBean("movie2", Movie.class);
-        film2.actorDetails();
-        Movie film3 = context.getBean("movie2", Movie.class);
-        film3.actorDetails();
-        //showing with scope as prototype
-        System.out.println("showing equality of beans using scope as prototype");
-        System.out.println(film2==film3);
-        Movie film4 = context.getBean("movie3", Movie.class);
-        film4.actorDetails();
-        Movie film5 = context.getBean("movie4", Movie.class);
-        film5.actorDetails();*/
-
     }
 }
