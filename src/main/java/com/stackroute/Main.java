ac3bdb2a0b221a8a847a8ade4ee455c16253d575
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 09:45:55 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
   public static void main(String[] args) {
+    //Using ApplicationContext for getting bean
     ConfigurableApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
     BeanLifecycleDemoBean bean=context.getBean("bean",BeanLifecycleDemoBean.class);
     BeanPostProcessorDemoBean beanpost=context.getBean("beanpost",BeanPostProcessorDemoBean.class);
