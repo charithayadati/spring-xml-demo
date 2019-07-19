58a31b61fc76af3668551c64fdd611b9f8982767
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 09:58:25 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -10,18 +10,17 @@ import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
-
+       //Using ApplicationContext
       ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
       Movie movie=context.getBean("movie",Movie.class);
       movie.setApplicationContext(context);
       movie.actorDetails();
-
+    //Using BeanFactory
       BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
       Movie movie1=beanFactory.getBean("movie",Movie.class);
       movie1.setBeanFactory(beanFactory);
       movie1.actorDetails();
-
-
+    //UsingBeanNameAware
         Movie movie2=context.getBean("movie",Movie.class);
         movie2.setBeanName("movie1");
         movie2.actorDetails();
