35cf27679f5bba645d3a2fcfd01a14fa5413484f
charithayadati <51942956+charithayadati@users.noreply.github.com>
2019-07-19 09:47:34 +0530

    Update BeanLifecycleDemoBean.java

--- src/main/java/com/stackroute/domain/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/domain/BeanLifecycleDemoBean.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.InitializingBean;
 
 public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
 
-
+        //Overriding the methods of interfaces mentioned
     public void afterPropertiesSet() throws Exception {
 
         System.out.println("Bean Initialized");
@@ -14,7 +14,7 @@ public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
 
         System.out.println("Bean Destroyed");
     }
-
+    //Creating methods for manually displaying the bean lifecycle
     public void customInit() throws Exception{
         System.out.println("Bean initialized using custom-init");
     }
